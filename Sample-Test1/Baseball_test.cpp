d7d51cff1790dc6853290efad812ba34843fed02
ahran_.choi <carokq@gmail.com>
2024-05-30 14:34:51 +0900

    [feature] 1

--- Sample-Test1/Baseball_test.cpp
+++ Sample-Test1/Baseball_test.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include "../Tdd_BaseballKATA/Baseball.cpp"
 
-TEST(BaseballGame, TryGameTest) {
-	EXPECT_EQ(1, 1);
+TEST(BaseballGame, ThrowExceptionWhenInputLengthIsUnmatched) {
+	Baseball game;
+	EXPECT_THROW(game.guess(string("12")), length_error);
 }
